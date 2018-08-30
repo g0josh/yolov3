0890901a91dc1c050d33af54fb5ce9ad1f52ce58
g0josh <jobin.jj@gmail.com>
2018-08-30 13:47:38 +1000

    Running but incorrect detection

--- darknet.py
+++ darknet.py
@@ -160,7 +160,6 @@ class Darknet(nn.Module):
         write = 0     #This is explained a bit later
         for i, module in enumerate(modules):        
             module_type = (module["type"])
-            print ("type = {}".format(module_type))
             if module_type == "convolutional" or module_type == "upsample":
                 x = self.module_list[i](x)
             elif module_type == "route":
@@ -202,7 +201,6 @@ class Darknet(nn.Module):
                 if not write:              #if no collector has been intialised. 
                     detections = x
                     write = 1
-
                 else:       
                     detections = torch.cat((detections, x), 1)
 
@@ -304,11 +302,9 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = False):
     num_anchors = len(anchors)
     
     anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
-    print ("stride = {}, grid size = {}, attrs = {}, n_anchors ={}, anchors = {}".format(stride, grid_size, bbox_attrs, num_anchors, anchors))
     prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
     prediction = prediction.transpose(1,2).contiguous()
     prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
-    print("Prediction size1 = {}".format(prediction.size()))
 
     #Sigmoid the  centre_X, centre_Y. and object confidencce
     prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
@@ -344,11 +340,11 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = False):
 
     prediction[:,:,:4] *= stride
    
-    print ("Resized prediction size = {}".format(prediction.size()))
+    print ("Transformed prediction size = {}".format(prediction.size()))
     return prediction
 
 def get_test_input(CUDA=False):
-    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.imread("images/dog-cycle-car.png")
     img = cv2.resize(img, (416,416))          #Resize to the input dimension
     img_ =  img[:,:,::-1].transpose((2,0,1))  # BGR -> RGB | H X W C -> C X H X W 
     img_ = img_[np.newaxis,:,:,:]/255.0       #Add a channel at 0 (for batch) | Normalise
@@ -369,10 +365,10 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
     batch_size = prediction.size(0)
 
     write = False
-
     for ind in range(batch_size):
+        print ("checking batch = {}".format(ind))
         image_pred = prediction[ind]          #image Tensor
-        #confidence threshholding 
+        #confidence thresholding 
         #NMS
         max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
         max_conf = max_conf.float().unsqueeze(1)
@@ -381,6 +377,7 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         image_pred = torch.cat(seq, 1)
 
         non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+        # print ("NON 0 ind = {}".format(non_zero_ind))
         try:
             image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
         except:
@@ -391,17 +388,103 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         #as scalars are supported in PyTorch 0.4
         if image_pred_.shape[0] == 0:
             continue
-            
+
         #Get the various classes detected in the image
         img_classes = unique(image_pred_[:,-1]) # -1 index holds the class index
+        # print ("classes = {}".format(img_classes))
+        for cls in img_classes:
+            #perform NMS
+            #get the detections with one particular class
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+            image_pred_class = image_pred_[class_mask_ind].view(-1,7)
 
+            #sort the detections such that the entry with the maximum objectness
+            #confidence is at the top
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)   #Number of detections
+
+            for i in range(idx):
+                #Get the IOUs of all boxes that come after the one we are looking at 
+                #in the loop
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                except ValueError:
+                    break
+
+                except IndexError:
+                    break
+
+                #Zero out all the detections that have IoU > treshhold
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i+1:] *= iou_mask       
+
+                #Remove the non-zero entries
+                non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+
+                batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)      
+                #Repeat the batch_id for as many detections of the class cls in the image
+                seq = batch_ind, image_pred_class
+
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+    try:
+        return output
+    except:
+        return 0
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+    
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes 
+    
+    
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+    #Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+ 
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
 
 if __name__ == '__main__':
     model = Darknet("cfg/yolov3.cfg")
     model.load_weights("yolov3.weights")
-    exit()
+    model.cuda()
+    model.eval()
     # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    inp = get_test_input(CUDA=False)
+    inp = get_test_input(CUDA=True)
     # print ("Cuda is available = {}".format(torch.cuda.is_available()))
-    pred = model(inp, CUDA=False)
+    print("image = {}\nshape={}".format(inp, inp.shape))
+    pred = model(inp, CUDA=True)
     print ("pred = {}\nsize = {}".format(pred, pred.size()))
+    result = write_results(pred, 0.5, 80, 0.4)
+    print ("results = {}\nSze = {}".format(result, result.shape))
